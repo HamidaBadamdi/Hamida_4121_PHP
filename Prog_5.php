2290dd553d4d5eccb93799f3cc0a66216557a9c7
Hamida Badamdi <hamida.badamdi143121@marwadiuniversity.ac.in>
2025-08-09 13:48:32 +0530

    Print 5 to 10 Uisng For & Foreach

--- Prog_5.php
+++ Prog_5.php
@@ -1,5 +1,5 @@
 <?php
-
+    //5.PHP programto print 5 to 10 using For and ForEach.
     //Using for loop to print 5 to 10...
     echo "<center><h1>Using for loop print(5 To 10) ...<br></h1>";
     for($i=5; $i<=10; $i++)
@@ -14,5 +14,6 @@
         echo "<h3>" ,$num , "</h3>";
     }
     echo "</center><hr>";
+
 
 ?>
