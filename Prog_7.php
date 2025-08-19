3070c4a2e544e00332fbeb437248dea9b6e87895
Hamida Badamdi <hamida.badamdi143121@marwadiuniversity.ac.in>
2025-08-19 21:55:41 +0530

    include and require function...

---
+++ Prog_7.php
@@ -0,0 +1,26 @@
+<?php
+     require("header.php");
+    
+?>
+            
+                            
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+</head>
+<body style="background:lightblue;">
+    <center>
+        
+        <h1>This is the home page...</h1>
+        <p>This page contains the main content.</p>
+
+        <?php
+            include("footer.php");
+           
+        ?>
+</center>    
+</body>
+</html>
