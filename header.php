6f8004227699d6f046f083ababc5373c243ed3d8
Hamida Badamdi <hamida.badamdi143121@marwadiuniversity.ac.in>
2025-08-19 21:53:55 +0530

    Header File For the Website

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+</head>
+<body>
+    
+    <header style="background:#f2f2f2; padding:10px; text-align:center;">
+        <h1>Welcome, to my website...</h1><hr>
+</header>
+</body>
+</html>
